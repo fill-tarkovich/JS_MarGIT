a5499b20fff291db52929c824cd9d085ab55e027
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-01-27 10:21:59 +0200

    task4 added

---
+++ Task4/task4.js
@@ -0,0 +1,10 @@
+// for (let number = 0; number <= 12; number += 2) {
+//   console.log(number);
+// }
+
+for (let current = 20; ; current++) {
+  if (current % 7 == 0) {
+    console.log(current);
+    break;
+  }
+}
