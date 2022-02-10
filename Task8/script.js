6e185bfe851a3fd3fa4fbd92614cf77f8477a359
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-02-10 15:03:15 +0200

    commit

--- Task8/script.js
+++ Task8/script.js
@@ -1,4 +1,5 @@
 let btnAdd = document.querySelector("#add");
+let resultTable = document.querySelector(".result");
 const carArray = [];
 class Car {
   constructor(licence, maker, model, owner, price, color) {
@@ -12,15 +13,33 @@ class Car {
 }
 
 btnAdd.addEventListener("click", () => {
-  const car = {};
-  let licence = document.querySelector("#licence");
+  let licence = document.querySelector("#licence").value;
   let maker = document.querySelector("#maker").value;
   let model = document.querySelector("#model").value;
   let owner = document.querySelector("#owner").value;
-  let price = document.querySelector("#price").value;
+  let price = Number(document.querySelector("#price").value);
   let color = document.querySelector("#color").value;
 
   let newCar = new Car(licence, maker, model, owner, price, color);
   carArray.push(newCar);
-  console.table(carArray);
+  makeTable();
 });
+
+const makeTable = () => {
+  let result = "<table border=1 >";
+  result += "<tr>";
+  for (const key in carArray[0]) {
+    result += "<th>" + key + "</th>";
+  }
+  result += "</tr>";
+  for (let i = 1; i < carArray.length; i++) {
+    result += "<tr>";
+    for (const key in carArray[i]) {
+      result += "<td>" + carArray[i][key] + "</td>";
+    }
+    result += "</tr>";
+  }
+  result += "</table>";
+  resultTable.innerHTML = result;
+};
+makeTable();
