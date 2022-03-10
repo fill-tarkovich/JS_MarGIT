6b927bd5102d83d60365f65cb0f60067c7abd7d9
Filipp Tarkovich <fill.tarkovich@gmail.com>
2022-03-10 14:58:59 +0200

    refactor script.js

--- PokeApp/script.js
+++ PokeApp/script.js
@@ -1,39 +1,29 @@
-let cards = document.querySelector(".cards");
-
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=153`)
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=154`)
   .then((response) => response.json())
   .then(function (allpokemon) {
     allpokemon.results.forEach(function (pokemon) {
       fetch(pokemon.url)
         .then((response) => response.json())
         .then(function (pokemonData) {
-          renderPokemon(pokemonData);
+          let pokemonContainer = document.createElement("div");
+          pokemonContainer.classList.add("card");
+          let pokemonName = document.createElement("h3");
+          pokemonName.innerText = pokemonData.name;
+          let pokemonTypes = document.createElement("ul");
+          let types = pokemonData.types;
+          types.forEach(function (type) {
+            let typeLi = document.createElement("li");
+            typeLi.innerText = type["type"]["name"];
+            pokemonTypes.append(typeLi);
+          });
+          let pokemonImage = document.createElement("img");
+          pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonData.id}.png`;
+          pokemonContainer.append(pokemonImage, pokemonName, pokemonTypes);
+          document.querySelector(".cards").appendChild(pokemonContainer);
         });
     });
   });
 
-function renderPokemon(pokemonData) {
-  let cards = document.querySelector(".cards");
-  let pokemonContainer = document.createElement("div");
-  pokemonContainer.classList.add("card");
-  let pokemonName = document.createElement("div");
-  pokemonName.classList.add("card_title");
-  pokemonName.innerText = pokemonData.name;
-  let pokemonTypes = document.createElement("ul");
-  createTypes(pokemonData.types, pokemonTypes);
-  let pokemonImage = document.createElement("img");
-  pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonData.id}.png`;
-  pokemonContainer.append(pokemonImage, pokemonName, pokemonTypes);
-  cards.appendChild(pokemonContainer);
-}
-
-function createTypes(types, ul) {
-  types.forEach(function (type) {
-    let typeLi = document.createElement("li");
-    typeLi.innerText = type["type"]["name"];
-    ul.append(typeLi);
-  });
-}
 // Margit's version
 
 // const pokedex = document.querySelector(".cards");
